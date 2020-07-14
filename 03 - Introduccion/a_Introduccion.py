ef10543f6953359165ece9474bb986fe9e91a7e5
Rodman Iñiguez <rodman.iniguez@epn.edu.ec>
2020-07-14 09:01:28 -0500

    Spider

---
+++ 03 - Introduccion/a_Introduccion.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+print("Hola Spider")
+
+nombre = "Rodman"
+numero = 6
+lista = [1, 2, 3]
+tupla = (4, 5, 6)
+diccionario = {
+    "nombre": "Rodman"
+    "apellido": "Iñiguez"
+    }
